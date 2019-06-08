d3b0e96209d318a7464f4d9074455d026321a434
Afonso Silva <ajcerejeira@gmail.com>
2019-06-08 16:29:14 +0200

    Prepared Pypi deploy

--- setup.py
+++ setup.py
@@ -7,24 +7,22 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="vat-validator",
-    version="0.0.1",
+    version="0.1.0",
     author="Afonso Silva",
     author_email="ajcerejeira@gmail.com",
     description="VAT validation library",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/ajcerejeira/vatval",
-    license='MIT License',
+    license="MIT License",
     packages=setuptools.find_packages(),
-    test_suite='tests',
+    test_suite="tests",
     classifiers=[
         "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Topic :: Office/Business :: Financial',
+        "Topic :: Office/Business :: Financial",
     ],
-    install_requires=[
-        "zeep",
-    ]
+    install_requires=["zeep"],
 )
