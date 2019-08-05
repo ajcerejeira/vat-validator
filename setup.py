5e9816c055f928630cab77ae56b0cab83d705382
Afonso Silva <ajcerejeira@gmail.com>
2019-08-05 18:18:56 +0100

    Migrated configuration from setup.py to setup.cfg. Tox settings are now in setup.cfg as well

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-import setuptools
+from setuptools import setup
 
-
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
-
-setuptools.setup(
-    name="vat-validator",
-    version="0.1.1",
-    author="Afonso Silva",
-    author_email="ajcerejeira@gmail.com",
-    description="VAT validation library",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/ajcerejeira/vat-validator",
-    license="MIT License",
-    packages=setuptools.find_packages(),
-    test_suite="tests",
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Financial and Insurance Industry",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Office/Business :: Financial",
-        "Topic :: Security :: Cryptography",
-        "Topic :: Utilities",
-        "Typing :: Typed",
-    ],
-    install_requires=["typing;python_version<'3.5'"],
-)
+setup()
